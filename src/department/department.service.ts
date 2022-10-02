8085afbad952ce7f555831255ae5f3e5fe5f3e50
AnLG <anluugia@gmail.com>
2022-10-02 12:04:11 +0700

    Implement a decorator to catch TypeORM queryFailedError

--- src/department/department.service.ts
+++ src/department/department.service.ts
@@ -1,10 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { IlistQueryOptions } from './department.constant';
 import Department, { DEPARTMENT_STATUS } from './department.entity';
 import { CreateDepartmentDto } from './dtos/create-department.dto';
+import { catchFailedQueryClass } from '../non-modules/decorators/catch-query-failed.decorator';
 
 @Injectable()
+@catchFailedQueryClass(__filename)
 export class DepartmentService {
   constructor(
     // Tell the DI system to know what to inject here
@@ -24,5 +27,46 @@ export class DepartmentService {
     const created = await this.departRepo.save(toBeCreatedDepartment);
 
     return created;
+  }
+
+  async findOne(id: string): Promise<Department> {
+    return this.departRepo.findOne({
+      where: {
+        id,
+      },
+    });
+  }
+
+  // @catchFailedQuery(__filename)
+  async findMany(
+    queryOptions: IlistQueryOptions,
+  ): Promise<{ count: number; data: Department[] }> {
+    const { skip, take, searchTerm, status } = queryOptions;
+
+    const list = await this.departRepo
+      .createQueryBuilder('DEPARTMENT')
+      .offset(skip)
+      .limit(take)
+      .orderBy('DEPARTMENT.createdAt', 'DESC')
+      .where(
+        // eslint-disable-next-line max-len
+        '(UPPER(DEPARTMENT.name) like UPPER(:name) OR UPPER(DEPARTMENT.idCode) like UPPER(:name)',
+        {
+          name: `%${searchTerm || ''}%`,
+        },
+      )
+      .andWhere(`DEPARTMENT.status ${status ? '= :status' : 'IS NOT NULL'}`, {
+        status,
+      })
+      .getManyAndCount();
+
+    return {
+      count: list[1],
+      data: list[0],
+    };
+  }
+
+  getSomeNumber() {
+    return 10;
   }
 }
---
+++ src/non-modules/decorators/catch-query-failed.decorator.ts
@@ -0,0 +1,73 @@
+import { HttpException } from '@nestjs/common';
+import { QueryFailedError } from 'typeorm';
+
+export const catchFailedQuery = (currentFilePath) => {
+  return function (
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor,
+  ) {
+    const actualFunction = descriptor.value;
+    descriptor.value = async function (...args) {
+      try {
+        const result = await actualFunction.apply(this, args);
+        return result;
+      } catch (QueryFailedError) {
+        const exception = new HttpException(
+          {
+            type: 'Failed TypeORM query',
+            query: QueryFailedError.query,
+            message: QueryFailedError.message,
+            failedAt: currentFilePath,
+          },
+          500,
+        );
+        console.log(exception);
+        throw exception;
+      }
+    };
+    return descriptor;
+  };
+};
+
+// Only used for service querying using TypeORM
+// THis function loop through a class methods
+// and apply a try catch to catch TypeORM queryFailedError
+// The error will be converted to TypeORM's HttpException
+export const catchFailedQueryClass = (filePath) => {
+  return function (target: any) {
+    for (const propertyName of Object.getOwnPropertyNames(target.prototype)) {
+      if (propertyName === 'constructor') continue;
+      // Mutating the descriptor has no effect on the original object
+      // SO that when finished, use Object.defineProperty
+      const descriptor = Object.getOwnPropertyDescriptor(
+        target.prototype,
+        propertyName,
+      );
+      const isAsyncMethod =
+        descriptor.value.constructor.name === 'AsyncFunction';
+      if (!isAsyncMethod) continue;
+      const originalMethod = descriptor.value;
+      descriptor.value = async function (...args) {
+        try {
+          const result = await originalMethod.apply(this, args);
+          return result;
+        } catch (QueryFailedError) {
+          const exception = new HttpException(
+            {
+              type: 'Failed TypeORM query',
+              query: QueryFailedError.query,
+              message: QueryFailedError.message,
+              failedAt: filePath,
+            },
+            500,
+          );
+          console.log(exception);
+          throw exception;
+        }
+      };
+      Object.defineProperty(target.prototype, propertyName, descriptor);
+    }
+    return target;
+  };
+};
